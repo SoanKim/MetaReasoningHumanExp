b18c2a1d07a0053fa966767b032983524c3512de
Soan Kim <39689481+SoanKim@users.noreply.github.com>
2025-03-06 15:04:15 +0100

    It works perfectly on 90 trials

--- main.py
+++ main.py
@@ -7,76 +7,49 @@
 from humanData import *
 from treeSearch import MCTS
 import time
+from utils import plotLearning, makeGif
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
 from matplotlib.pyplot import text
 
 data = df1Subj1
+scoreHistory = []
+qTables = []
+ucbTables = []
+freqTables = []
 
-num_agents = 2
-num_trials = len(data)
-avgScores = []
-
-
-startTime = time.time()
-print("start_time:", startTime)
-totalScores = []
 score = 0
-for agent in range(num_agents):
-    scores = np.zeros((num_agents, 4))
-    for prbIdx in range(4):
-        print(f"************************* new problem starts: #{prbIdx} *****************************")
-        TS = MCTS(prbIdx)
-        TS.traverse(node=None)
-        bestChild1 = TS.getBestChild(node=None)
-        print(f"bestChild'sCurrent: {bestChild1.current}")
-        finalReward = TS.traverse(node=bestChild1)
+for prbIdx in range(90):
+    print(f"************************* new problem starts: #{prbIdx} *****************************")
+    TS = MCTS(prbIdx)
+    TS.traverse(node=None)
+    bestChild1 = TS.getBestChild(node=None)
+    finalReward = TS.traverse(node=bestChild1)
+    print("finalReward", finalReward)
 
-        endTime = time.time()
-        print(f'Job took for trial {prbIdx}: {round(endTime - startTime, 3)} sec')
-
-        print(f"@@@@@@@@@@ Reward of the trial: {finalReward} @@@@@@@@@@@")
-        scores[agent][prbIdx] += finalReward
-
-        score += np.mean(scores) / (prbIdx + 1)
-        totalScores.append(score)
-print(totalScores)
+    score += finalReward
+    scoreHistory.append(score)
+    qTables.append(MCTS.qTable.copy())
+    ucbTables.append(TS.ucbTable.copy())
+    freqTables.append(TS.freqTable.copy())
 
 
-# print(f">>>>>>>>>>>>>>>>> Total score: {scores} <<<<<<<<<<<<<<<<<<<<<")
-# fig = plt.figure()
-# plt.plot(all_scores)
-# plt.title("Accuracy of Monte Carlo Tree Search")
-# plt.axvline(60, 0,1, linestyle='--', color='r')
-# text(50, 0.8, "TRAIN", rotation=0, verticalalignment='center')
-# text(61, 0.8, "TEST", rotation=0, verticalalignment='center')
-# plt.ylim([0, 1])
-# fig.savefig('temp.png')
+makeGif(qTables, "Q")
+makeGif(ucbTables, "UCB")
+makeGif(ucbTables, "Visits")
+
+# imgs = [Image.matshow(img) for img in qTables]
+# # duration is the number of milliseconds between frames; this is 40 frames per second
+# imgs[0].save("array.gif", save_all=True, append_images=imgs[1:], duration=50, loop=0)
+
+print("avgScoreHistory", scoreHistory)
+fileName = 'MCTSresult.png'
+plotLearning(scoreHistory, filename=fileName, x=90)
 
 
-
-
-
-#     if timeStep == 0: --> replace it with nodeid
-    #         element += random.choice(range(3))
-    #         game.move(nodeID, element)
-    #     else:
-    #         legalMoves = game.legalMove(element)
-    #         action = random.choice(legalMoves)
-    #         nextState = game.move(nodeID, action)
-    #
-    # dimChoice = random.choice(range(4))
-    # candidates = cardAvail[element][dimChoice]
-    # if not candidates:
-    #     finalAnswer = 0
-    # else:
-    #     finalAnswer = random.choice(candidates)
-    # if finalAnswer == answer:
-    #     score += 1
-    # else:
-    #     score += 0
-    #
-    # scores.append(np.round(score/(prbIdx+1), 2))
-
-# avgScores = np.mean(scores, axis=0)
-
-
+# startTime = time.time()
+# #print("start_time:", startTime)
+# endTime = time.time()
+# print(f'Job took for trial {prbIdx}: {round(endTime - startTime, 3)} sec')
--- treeSearch.py
+++ treeSearch.py
@@ -16,6 +16,7 @@ class MCTS:
     visits = defaultdict(lambda: 0)  # please initialize this every trial
     qTable = np.zeros((3, 5))
     freqTable = np.zeros((3, 5))  # prob of successful visits
+    metaTable = np.zeros((3, 5))
 
     def __init__(self, prbIdx):
         # initialize game
@@ -30,9 +31,9 @@ class MCTS:
 
         # hyperparam
         self.gamma = 1
-        self.exploreConstant = 2
+        self.exploreConstant = 100
 
-        # root node should be executed inside of MCTS to get updated.
+        # root node should be executed inside MCTS to get updated.
         self.root = Node(current=None, parent=None)
         self.ucbTable = np.full((3, 5), np.inf)
 
@@ -77,8 +78,7 @@ class MCTS:
 
     def selectArm(self, node):
         state, prob = self.getState(node)
-        print(f"state {state}")
-        print(f"prob {prob}")
+        print(f"state & prob: {state} {prob}")
         assert len(state) == len(prob), "The length of state and action doesn't match"
         action = self.argMaxAction(state, prob)
         return action
@@ -86,7 +86,7 @@ class MCTS:
     def updateQ(self, current, reward):
         # Guez et al., 2012
         delta = reward - MCTS.qTable[current] / MCTS.visits[current]
-        print(f">>>>>> CURRENT, REWARD, VISIT, & DELTA: {current}, {round(reward, 3)}, {MCTS.visits[current]}, {round(delta, 3)} <<<<<<<")
+        #print(f">>>>>> CURRENT, REWARD, VISIT, & DELTA: {current}, {round(reward, 3)}, {MCTS.visits[current]}, {round(delta, 3)} <<<<<<<")
         MCTS.qTable[current] += delta
 
     def updateUCB(self, current, node):
@@ -120,8 +120,6 @@ class MCTS:
 
         # append child to parent node
         node.parent.children.append(node)
-        #print("node.parent.children:", node.parent.children)
-        #print("visits\n", MCTS.visits)
         print("monitor\n", self.monitor)
         print("qTable\n", MCTS.qTable)
         print("freqTable\n", MCTS.freqTable)
@@ -129,26 +127,21 @@ class MCTS:
 
     def rollout(self, node):  # if you return a value, it stops after one iteration.
         bestAction = self.selectArm(node)
-        print(f"bestAction: {bestAction}")
+        #print(f"bestAction: {bestAction}")
         current = self.getCurrent(bestAction, parent=node)
-        print(f"current: {current}")
+        #print(f"current: {current}")
         child = Node(current=current, parent=node)
         leafVal = self.getLeafVal(current=current)
         self.backprop(reward=leafVal, node=child)
-        print(f"leafValue: {leafVal}")
+        #print(f"leafValue: {leafVal}")
         # return leafVal, current
 
     def traverse(self, node):
-        """
-        todo: select actions to the leaf node (depth 2) by greedy policy
-        rollout once --> visit += 1
-        :return: nothing. But update reward of each child.
-        """
         if node:
             node = node
         else:
             node = self.root
-        print(f"depth: {self.depth}")
+        #print(f"depth: {self.depth}")
         # This is the problem !!!!
         if self.depth == 0:
             while len(self.getState(node)[0]) > 0:  # If you change while into if, there's no child
@@ -166,14 +159,16 @@ class MCTS:
                 if len(self.getState(node)[0]) == 0:
                     break
                 else:
-                    print("len(self.getState(node)[0])", len(self.getState(node)[0]))
+                    #print("len(self.getState(node)[0])", len(self.getState(node)[0]))
                     newParent = self.getBestRoot()
-                    print(f" $$$$$$$$$$$$$ Going up for the best Parent!! $$$$$$$$$$$$", newParent.current)
+                    #print(f" $$$$$$$$$$$$$ Going up for the best Parent!! $$$$$$$$$$$$", newParent.current)
                     self.rollout(newParent)
                     bestChild = newParent.children[-1]
-                    finalReward = self.leafState[bestChild.current[0]*4+bestChild.current[1]-1]  #self.leafState[bestChild.current[0]*4+bestChild.current[1]-1]
-                    print(f"finalReward: {finalReward}")
-                    print(f"newParent.current: {newParent.current}")
+                    print("best child.current", bestChild.current)
+                    finalReward = self.leafState[bestChild.current[0]*4+bestChild.current[1]-1]
+                    # finalReward = self.selectCard(bestChild)
+                    #print(f"finalReward: {finalReward}")
+                    #print(f"newParent.current: {newParent.current}")
                     finalR += finalReward
                 self.depth += 1
                 return finalR
@@ -191,8 +186,7 @@ class MCTS:
         else:
             maxUCB = np.argmax(self.ucbTable[node.current[0], 1:]) + 1
             maxUCBposition = (node.current[0], maxUCB)
-        print(f"maxUCB: {maxUCB}")
-        print(f"maxUCBposition: {maxUCBposition}")
+        #print(f"maxUCB & maxUCB position: {maxUCB},{maxUCBposition}")
 
         for child in node.children:
             if child.current == maxUCBposition:
@@ -217,7 +211,7 @@ class MCTS:
             if bestRoot2 is not None:
                 bestRootLi.append(bestRoot2.current[0])
                 self.traverse(node=bestRoot2)
-        print("bestRootLi", bestRootLi)
+        #print("bestRootLi", bestRootLi)
         return bestRootLi
 
     def getCurrent(self, action, parent):
@@ -239,20 +233,21 @@ class MCTS:
 
     def getReward(self, cardChosen):
         reward = self.answer == cardChosen
-        print(f"answer: {self.answer}, cardChosen: {cardChosen}")
+        #print(f"answer: {self.answer}, cardChosen: {cardChosen}")
         return int(reward)
 
-    def selectCard(self, bestChildCurrent):
+    def selectCard(self, bestChild):
+        print("answer: ", self.answer)
         #assert self.depth == 3, f"You cannot select card before depth 3. current depth: {self.depth}"
         print("cards avail", self.cardAvail)
-        cardsAvail = self.cardAvail[bestChildCurrent[0]][bestChildCurrent[1] - 1]
+        cardsAvail = self.cardAvail[bestChild.current[0]][bestChild.current[1] - 1]
         if len(cardsAvail) == 0:
             print("There is no card available!")
             reward = 0
-            bestParent = np.argmax(self.monitor[:, 0])
-            for child in self.root.children:
-                if child.current == (bestParent, 0):
-                    self.traverse(child)
+            # bestParent = np.argmax(self.monitor[:, 0])
+            # for child in self.root.children:
+            #     if child.current == (bestParent, 0):
+            #         self.traverse(child)
 
             # actions = np.argwhere(self.monitor[bestParent, 1:] == 0).flatten() + 1
             # action = random.choice(actions)
@@ -265,5 +260,7 @@ class MCTS:
                 reward = self.getReward(cardChosen)
                 print(f"***********  reward: {int(reward)}  ***********")
             else:
-                reward = self.getLeafVal(bestChildCurrent)
+                reward = self.getLeafVal(bestChild.current)
+            self.updateQ(bestChild.current, reward)
+        # self.backprop(reward=reward, node=bestChild)
         return reward
